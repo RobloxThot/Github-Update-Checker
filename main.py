d8181463340ca996e4e6ee92441ff7bbe0f6e9b5
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-03-31 17:20:51 -0400

    Update main.py
    Added comments
    
    Fixed get commit msg
    
    Renamed all to fit python naming conventions
    
    Cleaned up unnecessary stuff
    
    Made getting hash only do one request and not two
    
    Made embed link the correct url
    
    Prob some other shit too

--- main.py
+++ main.py
@@ -1,64 +1,65 @@
-botToken = "x.x.x"
-channelId = 0
-url = "https://github.com/a/b"
+##########################
+#      Config below      #
+##########################
+
+bot_token = "x.x.x" # Bot token or set it to an env var
+
+channel_id = 0 # Channel ID to send the embed to
+
+url = "https://github.com/a/b" # Github url you want to check
+
+old = "(Bot/server was reset)" # What to say when you reset (you could edit this to save and load commmit ids too)
+
+##########################
+#      Script below      #
+##########################
 
 import requests
 import discord
-import time
 import re
 from discord.ext import tasks, commands
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix="!?",intents=intents)
 
-old = "(Bot/server was reset)"
-
-def getCommitHash():
+def get_commit_hash(): # Attempts to find the commit hash (iffy)
 	response = requests.get(url)
 	for line in response.text.split("\n"):
 		if "commit" in line and "fragment" in line:
 			commit = line.split("/")[4].split('"')[0]
 			return commit
 
-def getCommitMsg():
+def get_commit_msg(commit): # Grabs the commit message from the patch file
 	rq = requests.get(f'{url}/commit/{commit}.patch')
 	match = re.search(r'^Subject: \[PATCH\] (.*)$', rq.text, re.MULTILINE)
-	commitMsg = match.group(1) if match else '⚠️ Failed to get update msg'
+	return match.group(1) if match else '⚠️ Failed to get update msg'
 
 @client.event
 async def on_ready():
-	await client.change_presence(activity=discord.Game(name="Looking for update :eyes:"))
 	print("Bot is ready!")
-	await my_loop.start()
+	await check_loop.start()
 
-@client.command(pass_context=True)
-async def ping(ctx):
-	await ctx.send("> `Pong! " + str(round(client.latency * 1000)) + "ms`")
-
-@tasks.loop(seconds=60)
-async def my_loop():
-	global b,old
-	time.sleep(10)
+@tasks.loop(seconds=60) # Set this to how often to check but remeber GH has a ratelimit
+async def check_loop():
+	global old
 	print("Checking...")
-	fullHash = getCommitHash()
-	new = getCommitHash()[:7]
+	full_hash = get_commit_hash()
+	new = full_hash[:7] # Shorten the hash to 7 long to match GitHub's commit history
 	if new != old:
-		print("New update hash: ",fullHash)
-		channel = client.get_channel(channelId)
+		print("New update hash: ",full_hash)
+		channel = client.get_channel(channel_id)
 		embed = discord.Embed(title="Repo updated:", description= "", color=0x7289da)
 		embed.add_field(name="Old Commit", value="``"+old+"``", inline=True)
 		embed.add_field(name="Last Commit", value="``"+new+"``", inline=True)
-		embed.add_field(name="Commit msg", value="``"+getCommitMsg()+"``",inline=False)
-		embed.add_field(name="Commit changes", value=f'[Link](https://github.com/7GrandDadPGN/VapeV4ForRoblox/commit/{fullHash})')
+		embed.add_field(name="Commit msg", value="``"+get_commit_msg(full_hash)+"``",inline=False)
+		embed.add_field(name="Commit changes", value=f'[Link]({url}/commit/{full_hash})')
 		old = new
 		await channel.send(embed=embed)
 	else:
 		print("Nothin new")
 
-@my_loop.before_loop
+@check_loop.before_loop
 async def before_some_task():
   await client.wait_until_ready()
 
-
-
-client.run(botToken)
+client.run(bot_token)
 #pyinstaller --clean --onefile main.py
